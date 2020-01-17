c475e792073f9fad28d5350c3734846e2d58d469
elsa-beeb <erojasbar@unprg.edu.pe>
2020-01-17 13:21:24 -0500

    se agregó appy08.py

---
+++ elsa_rojas/libreria.py
@@ -0,0 +1,68 @@
+#ejercicio 01
+def producto(pr):
+    if(pr=="avena"):
+        return pr.title()
+    else:
+        return pr
+#FIN
+
+#ejercicio 02
+def bebe(str):
+    if(str=="NIÑA"):
+        return str.lower()
+    else:
+        return str
+#FIN
+
+#ejercicio 03
+def bebe(br):
+    if(br=="ruth"):
+        return br.lstrip()
+    else:
+        return br
+#FIN
+
+#ejercicio 04
+def pequeña(cad):
+    if(cad=="niño"):
+        return cad.upper()
+    else:
+        return cad
+#FIN
+
+#ejercicio 05
+def flores(fl):
+    if(fl=="rosalia"):
+        return fl.replace()
+    else:
+        return fl
+#FIN
+
+#ejercicio 06
+def yuyo(yu):
+    if(yu=="mosquito"):
+        return yu.rstrip()
+    else:
+        return yu
+#FIN
+
+
+#ejercicio 07
+def riycr(tu):
+    if(tu=="0727atv"):
+        return tu.isalnum()
+    else:
+        return tu
+#FIN
+
+#ejercicio 08
+def maria(vc):
+    if(vc=="2707"):
+        return vc.isdigit()
+    else:
+        return vc
+#FIN
+
+
+
+
---
+++ elsa_rojas/test.py
@@ -0,0 +1,40 @@
+#ejercicio 01
+assert (libreria.producto("avena")=="avena")
+assert (libreria.producto("arroz")=="arroz")
+print("producto")
+
+#EJERCICIO 02
+assert (libreria.pequeño("NIÑA")=="niña")
+assert (libreria.pequeño("Yutsely")=="Yutsely")
+print("bebe")
+
+#EJERCICIO 03
+assert (libreria.bebe("ruth")=="ruth")
+assert (libreria.bebe("david")=="david")
+print("niño")
+
+#EJERCICIO 04
+assert (libreria.pequeña("niño")=="NIÑO")
+assert (libreria.pequeña("albert")=="albert")
+print("pequeño")
+
+#EJERCICIO 05
+assert (libreria.flores("rosalia")=="rosalia")
+assert (libreria.flores("alberto")=="alberto")
+print("flores")
+
+#EJERCICIO 06
+assert (libreria.yuyo("mosquito")=="mosquito")
+assert (libreria.bebe("noe")=="noe")
+print("yuyo")
+
+#EJERCICIO 07
+assert (libreria.riycr("4875A")=="4875A")
+assert (libreria.riycr("458T")=="458T")
+print("riycr")
+
+#EJERCICIO 08
+assert (libreria.cool(" rosa ")==" rosa ")
+assert (libreria.cool(" belen ")==" belen ")
+print("cool")
+
